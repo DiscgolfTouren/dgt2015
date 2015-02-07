a2faf75678b9e20a277a339248c9ff5c989fcda9
Peter Bygde <peter.bygde@visma.se>
2015-02-07 12:12:20 +0100

    Skeleton

---
+++ DGTWebSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DGTWebSite.Startup))]
+namespace DGTWebSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
