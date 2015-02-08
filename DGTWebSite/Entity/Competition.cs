cfc9d13101074bcb75f14948e05ed06ada14a0d3
mikaedva <mikaedva@gmail.com>
2015-02-08 16:23:06 +0100

    models and mappings

---
+++ DGTWebSite/Entity/Competition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DGTWebSite.Entity
+{
+    public class Competition
+    {
+        public string Name { get; set; }
+        public IDictionary<Player, PlayerCompetitionStatus> Players { get; set; }
+        public DateTime Date { get; set; }
+        public Courseconfiguration Configuration { get; set; }
+    }
+}
