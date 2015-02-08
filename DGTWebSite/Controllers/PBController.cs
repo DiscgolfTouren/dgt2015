383f931a4269936f0a7ed0eee17c5f83a95b8817
Peter Bygde <peter.bygde@visma.se>
2015-02-08 17:12:39 +0100

    Labbsida PB

---
+++ DGTWebSite/Controllers/PBController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using DGTWebSite.Models;
+
+namespace DGTWebSite.Controllers
+{
+    public class PBController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(string name, int number = 1)
+        {
+            return View(new PB() {Name = "Namnet är " + name, Number = number});
+        }
+    }
+}
---
+++ DGTWebSite/Models/PB.cs
@@ -0,0 +1,8 @@
+﻿namespace DGTWebSite.Models
+{
+    public class PB
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+    }
+}
