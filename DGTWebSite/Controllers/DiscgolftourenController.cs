f24c36e7051d080d95f5e10e0af3c268526393b2
peterbygde <peter@bygde.se>
2015-02-16 22:44:26 +0100

    Minimum site ...

---
+++ DGTWebSite/Controllers/DiscgolftourenController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DGTWebSite.Controllers
+{
+    public class DiscgolftourenController : Controller
+    {
+        //
+        // GET: /Discgolftouren/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Om()
+        {
+            return View();
+        }
+
+        public ActionResult Kontakt()
+        {
+            return View();
+        }
+	}
+}
