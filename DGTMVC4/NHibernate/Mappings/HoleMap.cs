bfe4d98b0da62ce76f51cb40db15ba1c65615335
mikaedva <mikaedva@gmail.com>
2015-04-07 20:06:42 +0200

    new maps

---
+++ DGTMVC4/NHibernate/Mappings/HoleMap.cs
@@ -0,0 +1,19 @@
+﻿using DGTMVC4.NHibernate.Models;
+using FluentNHibernate.Mapping;
+
+namespace DGTMVC4.NHibernate.Mappings
+{
+    public class HoleMap : ClassMap<Hole>
+    {
+        public HoleMap()
+        {
+            Id(h => h.Id);
+            Map(h => h.Number);
+            Map(h => h.Par);
+            Map(h => h.Elevation);
+            Map(h => h.Length);
+            Map(h => h.Description);
+        }
+         
+    }
+}
---
+++ DGTMVC4/NHibernate/Mappings/HoleScoreMap.cs
@@ -0,0 +1,16 @@
+﻿using DGTMVC4.NHibernate.Models;
+using FluentNHibernate.Mapping;
+
+namespace DGTMVC4.NHibernate.Mappings
+{
+    public class HoleScoreMap : ClassMap<HoleScore>
+    {
+        public HoleScoreMap()
+        {
+            Id(h => h.Id);
+            Map(h => h.HoleNumber);
+            Map(h => h.Score);
+        }
+         
+    }
+}
