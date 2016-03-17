5bf5efa20902f0bdb744bb8e64ee13385dd1e2ba
peterbygde <peter@bygde.se>
2016-03-17 23:03:24 +0100

    sponsorbilder

---
+++ DGTMVC4/App_Start/AuthConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//using Microsoft.Web.WebPages.OAuth;
+
+namespace DGTMVC4.App_Start
+{
+    public static class AuthConfig
+    {
+        public static void RegisterAuth()
+        {
+            // To let users of this site log in using their accounts from other sites such as Microsoft, Facebook, and Twitter,
+            // you must update this site. For more information visit http://go.microsoft.com/fwlink/?LinkID=252166
+
+            //OAuthWebSecurity.RegisterMicrosoftClient(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            //OAuthWebSecurity.RegisterTwitterClient(
+            //    consumerKey: "",
+            //    consumerSecret: "");
+
+            //OAuthWebSecurity.RegisterFacebookClient(
+            //    appId: "193685800998562",
+            //    appSecret: "d1a9b47fad28bafe343e3bde12fa95b5");
+
+            //OAuthWebSecurity.RegisterGoogleClient();
+        }
+    }
+}
